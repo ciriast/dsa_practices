03510b14855bf327ce653ec9acfd5fa745116617
ciriast <ciriastorres@gmail.com>
2026-06-07 09:55:15 -0600

    Finished binary search

---
+++ binary_search.py
@@ -0,0 +1,27 @@
+my_data = [2, 10, 100, 400, 500, 766, 700, 123, 456, 765, 98, 19, 45, 66, 255, 86]
+my_sorted_data = sorted(my_data)
+my_sorted_len = len(my_sorted_data)
+my_value = 765
+
+low = 0
+high = my_sorted_len - 1
+index = 0
+
+print(my_sorted_data)
+
+while low <= high:
+    mid = low + (high - low) // 2
+    
+    if my_sorted_data[mid] == my_value:
+        print("Found it! Index:", mid)
+        index = mid
+
+        break
+    
+    if my_sorted_data[mid] < my_value:
+        low = mid + 1
+    elif my_sorted_data[mid] > my_value:
+        high = mid - 1
+
+if index == 0:
+    print("No found it!")
