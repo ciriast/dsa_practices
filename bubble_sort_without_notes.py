36064839d2f058e833176369f11f44ef16880c5c
ciriast <ciriastorres@gmail.com>
2026-06-09 08:07:51 -0600

    Maybe I can do bubble sort with closed eyes

--- bubble_sort_without_notes.py
+++ bubble_sort_without_notes.py
@@ -1,1 +1,19 @@
 def bubble_sort(nums: list[int]):
+    end = len(nums)
+    swapping = True
+
+    while swapping:
+        swapping = False
+
+        for i in range(1, end):
+            if nums[i - 1] > nums[i]:
+                nums[i - 1], nums[i] = nums[i], nums[i - 1]
+                swapping = True
+
+
+        end -= 1
+
+
+my_list = [23, 576, 76, 12, 976, 345]
+bubble_sort(my_list)
+print(my_list)
