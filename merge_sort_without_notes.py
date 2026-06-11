14f13b3c925ab43d73c7dc3653015e34bf72794e
ciriast <ciriastorres@gmail.com>
2026-06-11 06:48:40 -0600

    Merge sort complete

--- merge_sort_without_notes.py
+++ merge_sort_without_notes.py
@@ -10,7 +10,6 @@ def merge_sort(nums: list[int]) -> list[int]:
 
     return merge(sorted_left, sorted_right)
 
-
 def merge(a: list[int], b: list[int]) -> list[int]:
     i = 0
     j = 0
@@ -19,12 +18,22 @@ def merge(a: list[int], b: list[int]) -> list[int]:
     final_list = []
 
     while i < a_len and j < b_len:
-        if nums[i] <= nums[j]:
-            final_list.append(nums[i])
+        if a[i] <= b[j]:
+            final_list.append(a[i])
             i += 1
         else:
-            final_list.append(nums[j])
+            final_list.append(b[j])
             j += 1
-
-
     
+    while i < a_len:
+        final_list.append(a[i])
+        i += 1
+
+    while j < b_len:
+        final_list.append(b[j])
+        j += 1
+
+    return final_list
+
+my_list = merge_sort([78, 12, 2, 98, 3555, 667, 234, 67, 88, 33, 127])
+print(my_list)
