4f32bcba6f9bc24d2ff07fb6e8bb1dad21c5fed6
ciriast <ciriastorres@gmail.com>
2026-06-15 07:09:18 -0600

    quick sort first version

--- quick_sort.py
+++ quick_sort.py
@@ -1,8 +1,28 @@
-def quick_sort(nums: list[int], first: int, last: int) -> list[int]:
-    pass
+def quick_sort(nums: list[int], first: int, last: int) -> None:
+    if last >= first:
+        return
+
+    pivot = partion(nums, first, last)
+
+    left = partion(nums, first, pivot - 1)
+    right = partion(nums, pivot + 1, last)
+
 
 def partion(nums: list[int], left: int, right: int):
-    pass
+    pivot = nums[right]
+    i = left - 1
 
-print("Continue with quick sort")
+    for j in range(i, right):
+        if nums[j] < pivot:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+    nums[i + 1], nums[right] = nums[right], nums[i + 1]
+
+
+    return i + 1
+
+my_nums = [2, 7, 19, 29, 477]
+quick_sort(my_nums, 0, 4)
+print(my_nums)
     
