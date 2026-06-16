bf2fb766e67d739ff88d0b609435007e2a0bc5f2
ciriast <ciriastorres@gmail.com>
2026-06-16 07:04:40 -0600

    My practice is not good, I need to improve

--- binary_search_16_june_2026.py
+++ binary_search_16_june_2026.py
@@ -1,8 +1,21 @@
 def binary_search(nums: list[int], my_key: int) -> int:
     low = 0
-    high = len(nums)
+    high = len(nums) - 1
 
-    for i in range(1, high):
+    while low <= high:
         mid = (low + high) // 2
 
-        if nums[mid]
+        if nums[mid] == my_key:
+            return mid
+
+        if nums[mid] < my_key:
+            low = mid + 1
+
+        if nums[mid] > my_key:
+            high = mid - 1
+    
+    return -1
+
+
+my_index = binary_search([1, 2, 4, 67, 89, 100, 120, 150], 1)
+print(my_index)
