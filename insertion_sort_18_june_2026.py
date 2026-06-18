51fd2e24c087b4a6bb4dd8163a0e8d55089618f9
ciriast <ciriastorres@gmail.com>
2026-06-18 08:13:11 -0600

    Good insertion sort, need to practice more, but it's ok!

--- insertion_sort_18_june_2026.py
+++ insertion_sort_18_june_2026.py
@@ -1,3 +1,15 @@
 def insertion_sort(nums: list[int]) -> list[int]:
-    start = 0
+    start = 1
     end = len(nums)
+
+    for i in range(start, end):
+        j = i
+
+        while j > 0 and nums[j - 1] > nums[j]:
+            nums[j - 1], nums[j] = nums[j], nums[j - 1]
+            j -= 1
+
+    return nums
+
+my_sorted_list = insertion_sort([60, 19, 345, 89, 9, 76, 21, 26, 777, 129, 52, 23, 72, 999, 222, 333, 1])
+print(my_sorted_list)
